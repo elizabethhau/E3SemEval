3f25c3a0a0952ddea9ee9103de75108b07f2be35
Emily Ahn <eahn@wellesley.edu>
2015-12-12 01:49:50 -0500

    testing functions within afinn. required pip install.

---
+++ test_Afinn.py
@@ -0,0 +1,13 @@
+# test AFINN
+
+from afinn import Afinn
+af = Afinn()
+af_emoticon = Afinn(emoticons=True)
+text1 = 'This is utterly excellent!'
+text2 = ':/'
+text3 = 'This is hardly excellent'
+print text1, ":", af.score(text1) #score = 3.0
+print "find_all:", af.find_all(text1) #['excellent']
+print text2, ":", af_emoticon.score(text2) #score = -2.0
+print text3, ":", af.score(text3)
+print "find_all:", af.find_all(text3) #['excellent']
