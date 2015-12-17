6bfa480c6601f74e603a83cc000bd76a088460f2
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2015-12-17 18:40:44 -0500

    Added comments

--- create_features.py
+++ create_features.py
@@ -1,18 +1,10 @@
 '''
 Authors: Elizabeth Hau, Emily Ahn, Emily Chen
 Date created: 12/12/2015
-Last modified: 12/14/2015
+Last modified: 12/17/2015
 *************************************************************
 This file reads in training and testing files, creates feature arrays, and 
 trains an SVM classifier to predict test tweets.
-
-TODO:
-    # create tagged files for PFTrainTag and PFTestTag
-    # find limit to utility of ALL CAPS feature
-    - new FT: presence of emoticons (use tagger an AFinn)
-    # confusion matrices
-    - documentation
-    - write-up
 *************************************************************
 '''
 
@@ -52,54 +44,76 @@ def apply_ft(array):
         full_ft.append(create_ft(tweet))
     return full_ft
             
-''' given 1 tweet (an array of tuples), return an array of 21 feature values.
-    takes sum of sentiment scores within entire tweet, and array at that sum 
-    index + 10 gets a feature of 1.
-    features are: 
-        - array[0] --> -10
-        - array[1] --> -9
-        ...
-        - array[20] --> 10
+''' This function creates all the features for a given tweet.
+    Given 1 tweet (an array of tuples), return an array of all feature values.
+    Features are:
+        - sentiment score (AFinn lexicon): 7 binary values
+        - number of words in ALL CAPS: 6 binary values
+        - emoticon score (AFinn Emoticon lexicon): 5 binary values
 '''
 def create_ft(tweet):
+    # afinn score for each word
     score = 0
-    afinn_ft = [0]*21 # initialize array of 0's
+    afinn_ft = [0]*7 # initialize array of 0's
     
+    # number of words in all caps
     num_all_caps = 0
     caps_ft = [0]*6
     
+    # afinn emoticon scores
+    emo_score = 0
+    emo_ft = [0]*5 
+    
     for (word,tag) in tweet:
-        score += af.score(word) #using the afinn score function
+        # word sentiments feature
+        score += af_emo.score(word) #using the afinn score function
+        
+        # all caps feature
         if is_all_caps(word):
             num_all_caps += 1
+        
+        # emoticon feature
+        if tag == 'E':
+            emo_score += af_emo.score(word)
             
-    #if scores are beyond -10 or 10 on either extreme, make them -10 or 10 respectively
-    if score < -10:
-        score = -10
-    elif score > 10:
-        score = 10
-    afinn_ft[int(score+10)] = 1
+    #if sum of the afinn scores for the tweet is beyond -3 or 3 on either extreme, 
+    # make them -3 or 3 respectively
+    if score < -3:
+        score = -3
+    elif score > 3:
+        score = 3
+    afinn_ft[int(score+3)] = 1
 
+    # if the number of words in all caps is more than 5, set it to 5
     if num_all_caps > 5:
         num_all_caps = 5
     caps_ft[int(num_all_caps)] = 1
-    #print "CAPS FT:\n",caps_ft
-    return afinn_ft+caps_ft
+    
+    #if sum of the afinn emoticon scores are beyond -2 or 2 on either extreme, 
+    # make them -2 or 2 respectively
+    if emo_score < -2:
+        emo_score = -2
+    elif emo_score > 2:
+        emo_score = 2
+    emo_ft[int(emo_score+2)] = 1
 
+    return afinn_ft + caps_ft + emo_ft
+
+''' This function checks if a given word is in all caps
+'''
 def is_all_caps(word):
     def isCap(x) : return x.isupper()
     result = filter(isCap, word)
-    #if len(result) == len(word):
-    #    print "ALL CAPS"
-    #    return
     return len(result) == len(word)
     
-''' read file of full tweets and grab the column of correct labels only '''
+''' This function reads a file of tweets and returns the column of 
+    correct labels as an array '''
 def get_labels(filename):
     return np.loadtxt(filename, delimiter="\t", usecols=(2,))
 
 if __name__ == "__main__":
-    af = Afinn(emoticons=True)
+    af = Afinn()
+    af_emo = Afinn(emoticons=True)
     
     train_tweets = full_array(readfile('data/cleaned/PFTrainTag.txt'))
     train_ft = apply_ft(train_tweets)
@@ -109,42 +123,70 @@ if __name__ == "__main__":
     test_ft = apply_ft(test_tweets)
     test_labels = get_labels('data/cleaned/PFTweetsTest.txt')
     
+    # count frequencies of train labels
+    count0 = [0,0,0]
+    for label in train_labels:
+        count0[int(label+1)] += 1
+    print "distribution in train[-1,0,1] :",count0
+    total = np.sum(count0)
+    print "% of 1s in train:",count0[2]/total #0.53053083768
+    print "% of 0s in train:",count0[1]/total #0.305922061982
+    print "% of -1s in train:",count0[0]/total #0.163547100338
+    
     # count frequencies of test labels
     count = [0,0,0]
     for label in test_labels:
         count[int(label+1)] += 1
-    print "distribution in test[-1,0,1] :",count
+    print "\ndistribution in test[-1,0,1] :",count
     total = np.sum(count)
     print "% of 1s in test:",count[2]/total #0.458218549128
     print "% of 0s in test:",count[1]/total #0.341597796143
     print "% of -1s in test:",count[0]/total #0.200183654729
-    
-    #print "ALL LABELS:", all_labels
-    classifier = svm.SVC(kernel='rbf', C=0.75) #gamma=0.7
+
+    classifier = svm.SVC(kernel='rbf', C=0.758) #gamma=0.7
     classifier.fit(train_ft, train_labels) # fit SVM on train data
     predict = classifier.predict(test_ft)
-    print "PREDICT:", predict
+    print "\nPREDICT:", predict
     
     # count frequencies of predicted labels
     count2 = [0,0,0]
     for label in predict:
         count2[int(label+1)] += 1
-    print "distribution in test[-1,0,1] :",count2
+    print "distribution in predicted[-1,0,1] :",count2
     total2 = np.sum(count2)
     print "% of 1s in predict:",count2[2]/total2 #0.904499540863
     print "% of 0s in predict:",count2[1]/total2 #0.0955004591368
     print "% of -1s in predict:",count2[0]/total2 #0.0
     
-    print "accuracy score of our predictions:",accuracy_score(test_labels, predict) #get accuracy: 0.474747474747
+    # Baseline: 0.458218549128 (proportion of label = 1)
+    # Accuracy with af score range [-10,10], all caps counting up to 6 words in all caps, no separate emo_ft: 0.474747474747
+    # Accuracy with af score range [-5,5], same all caps, emo_ft range [-2, 2] using afinn emoticon scores: 0.481175390266
+    # Accuracy with af score range [-3,3], same all caps, emo_ft range [-2, 2] using afinn emoticon scores: 0.483011937557
+    print "\naccuracy score of our predictions:",accuracy_score(test_labels, predict) 
     
     # Compute confusion matrix
     cm = confusion_matrix(test_labels, predict)
     np.set_printoptions(precision=2)
-    print('Confusion matrix, without normalization')
+    print('\nConfusion matrix, without normalization')
     print(cm)
-    # [[  0  40 178]
-    #  [  0  41 331]
-    #  [  0  23 476]]
-
-
     
+    #[[  0  67 151]
+    # [  0  60 312]
+    # [  0  33 466]]
+
+    percent_neg1_as_0 = cm[0][1]/np.sum(cm[0])
+    print "Among all the -1 labels, % incorrectly classified as 0:",percent_neg1_as_0
+    percent_neg1_as_1 = cm[0][2]/np.sum(cm[0])
+    print "Among all the -1 labels, % incorrectly classified as 1:",percent_neg1_as_1
+    percent_0_correct = cm[1][1]/np.sum(cm[1])
+    print "Among all the 0 labels, % classified correctly:", percent_0_correct 
+    percent_1_correct = cm[2][2]/np.sum(cm[2])
+    print "Among all the 1 labels, % classified correctly:", percent_1_correct
+    
+    # Compute normalized confusion matrix
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print('\nNormalized confusion matrix')
+    print(cm_normalized)
+    # [[ 0.    0.31  0.69]
+    #  [ 0.    0.16  0.84]
+    #  [ 0.    0.07  0.93]]
--- data/dataPreprocessing.py
+++ data/dataPreprocessing.py
@@ -1,14 +1,16 @@
 '''
 Authors: Elizabeth Hau, Emily Ahn, Emily Chen
 Date created: 11/24/2015
-Last modified: 12/12/2015
+Last modified: 12/17/2015
 *************************************************************
 This file contains all the data preprocessing
 *************************************************************
 '''
-
+from __future__ import division
 import readerAndWriter
 from ttp import ttp
+import numpy as np
+
 import CMUTweetTagger
 #import twokenize 
 
@@ -67,54 +69,54 @@ def deleteUnavailable(array):
 print "\n********************* TRAIN GOLD **********************************"
 trainGoldReadResult = readerAndWriter.readFile("trainC5gold.tsv")
 trainGoldLen = len(trainGoldReadResult)
-print "number of tweets in train gold before removing tweets that are 'not available':",trainGoldLen
+print "number of tweets in train gold before removing tweets that are 'not available':",trainGoldLen #6000
 
 trainGoldResult = deleteUnavailable(trainGoldReadResult)
 trainGoldResultLen = len(trainGoldResult)
-print "number of tweets in train gold after removing 'not available' tweets:",trainGoldResultLen
+print "number of tweets in train gold after removing 'not available' tweets:",trainGoldResultLen #5444
 
 trainGoldNumUnAvail = (trainGoldLen-trainGoldResultLen)
-print "Number of tweets unavailable in trainGold:", trainGoldNumUnAvail
+print "Number of tweets unavailable in trainGold:", trainGoldNumUnAvail #544
 
 print "\n********************* DEV GOLD **********************************"
 devGoldReadResult = readerAndWriter.readFile("devC5gold.tsv")
 devGoldLen = len(devGoldReadResult)
-print "number of tweets in dev gold before removing tweets that are 'not available':",devGoldLen
+print "number of tweets in dev gold before removing tweets that are 'not available':",devGoldLen #2000
 
 devGoldResult = deleteUnavailable(devGoldReadResult)
 devGoldResultLen = len(devGoldResult)
-print "number of tweets in dev gold after removing 'not available' tweets:",devGoldResultLen
+print "number of tweets in dev gold after removing 'not available' tweets:",devGoldResultLen #1814
 
 devGoldNumUnAvail = (devGoldLen-devGoldResultLen)
-print "Number of tweets unavailable in devGold:", devGoldNumUnAvail
+print "Number of tweets unavailable in devGold:", devGoldNumUnAvail #186
 
 print "\n********************* DEVTEST GOLD **********************************"
 devtestGoldReadResult = readerAndWriter.readFile("devtestC5gold.tsv")
 devtestGoldLen = len(devtestGoldReadResult)
-print "number of tweets in devtet gold before removing tweets that are 'not available':",devtestGoldLen
+print "number of tweets in devtet gold before removing tweets that are 'not available':",devtestGoldLen #2000
 
 devtestGoldResult = deleteUnavailable(devtestGoldReadResult)
 devtestGoldResultLen = len(devtestGoldResult)
-print "number of tweets in devtest gold after removing 'not available' tweets:",devtestGoldResultLen
+print "number of tweets in devtest gold after removing 'not available' tweets:",devtestGoldResultLen #1791
 
 devtestGoldNumUnAvail = (devtestGoldLen-devtestGoldResultLen)
-print "Number of tweets unavailable in devtestGold", devtestGoldNumUnAvail
+print "Number of tweets unavailable in devtestGold", devtestGoldNumUnAvail #209
 
 print "\n*********************** TOTAL *****************************************"
-print "Total tweets:",trainGoldResultLen+devGoldResultLen+devtestGoldResultLen
-print "Total distinct ids:", len(totalValidIds)
+print "Total tweets:",trainGoldResultLen+devGoldResultLen+devtestGoldResultLen #9049
+print "Total distinct ids:", len(totalValidIds) #9020
 
 print "\n********************* DEVTEST INPUT **********************************"
 devtestInputReadResult = readerAndWriter.readFile("devtestC5input.tsv")
 devtestInputLen = len(devtestInputReadResult)
-print "number of tweets in devtest input before removing tweets that are 'not available':",devtestInputLen
+print "number of tweets in devtest input before removing tweets that are 'not available':",devtestInputLen #2000
 
 devtestInputResult = deleteUnavailable(devtestInputReadResult)
 devtestInputResultLen = len(devtestInputResult)
-print "number of tweets in devtest input after removing 'not available' tweets:",devtestInputResultLen
+print "number of tweets in devtest input after removing 'not available' tweets:",devtestInputResultLen #1794
 
 devtestInputNumUnAvail = (devtestInputLen-devtestInputResultLen)
-print "Number of tweets unavailable in devtest input:", devtestInputNumUnAvail
+print "Number of tweets unavailable in devtest input:", devtestInputNumUnAvail #206
 '''
 
 ''' create new .tsv files to actually use for training, dev, and devtest '''
@@ -162,9 +164,8 @@ The files on a 3 point scale will be named as: <fileName>3pt.tsv
 
 '''
     This function takes in the original data on a 5 point scale
-    as an array and changes '''
-#lang_list = ['ar', 'cz', 'in']
-#langs = {lang: read_csv(lang+'_formants.csv') for lang in lang_list}
+    as an array and changes 
+'''
 def collapseScales(originalData):
     # newFile is an array of arrays where each element in newFile 
     # is a line (as an array)
@@ -177,8 +178,29 @@ def collapseScales(originalData):
             line[2] = str(1)
         newFile.append(line)
     return newFile 
-'''
+
+''' ******************* COLLAPSE SCALES ***************************'''
 originalFile = readerAndWriter.readFile("cleaned/allTopics.tsv")
+
+# find the distribution of the labels before collapsing the scales
+dist_count = [0]*5
+for element in originalFile:
+    line = element.split("\t")
+    dist_count[int(line[2])+2] += 1
+
+print "Distribution count of numbers [-2, -1, 0, 1, 2]:",dist_count #[139, 1086, 2667, 4608, 549]
+print "Total number of tweets",np.sum(dist_count) #9049
+distribution = [0]*5
+for num in range(len(dist_count)):
+    print "num is", num
+    print dist_count[num]
+    distribution[num] = dist_count[num]/np.sum(dist_count)
+    
+print "Distribution of numbers [-2, -1, 0, 1, 2] (in %):", distribution 
+#[0.015360813349541386, 0.12001326113382695, 0.29472869930379048, 0.50922753895458062, 0.06066968725826058]
+
+    
+'''
 result = collapseScales(originalFile)
 readerAndWriter.writeToFile("cleaned/allTopics3pt.tsv",result)
 
@@ -194,11 +216,60 @@ originalDevtest = readerAndWriter.readFile("cleaned/devtestGold.tsv")
 devtestReadResult = collapseScales(originalDevtest)
 readerAndWriter.writeToFile("cleaned/devtestGold3pt.tsv", devtestReadResult)
 '''
+
 '''
 *************************************************************
-This section of the file parses the raw tweet
+This section of the file tags the raw tweet
 *************************************************************
 '''
+
+''' This function takes in a list of tweets, tags each word in
+    the tweet as a triple in the form of (word, tag, confidence level)
+    and returns a list of tuples in the form of (word, tag) 
+    with URLs, user mentions, numbers, and hashtags removed
+'''
+def tag_tweets(tweets):
+    tagResults = CMUTweetTagger.runtagger_parse(tweets)
+    result = []
+    for tweet in tagResults: #w = word, t = tag, c = confidence level
+        tuplesList = []
+        for triple in tweet:
+            (w, t, c) = triple
+            #removing urls, user mentions, numbers, and hashtags from the tweet
+            if t != 'U' and t != '@' and t!= '$' and t != '#':
+                tuplesList.append((w,t))
+        result.append(tuplesList)
+        
+    return result
+
+''' This function takes in an array with tweet IDs, topics, sentiment
+    labels, tweets and returns an array of tagged tweets with URLs, 
+    user mentions, numbers, and hashtags removed
+'''
+def replaceTaggedTweet(data):
+    result = []
+    for element in data:
+        line = element.split("\t")
+        tweet = [line[3]]
+        cleaned = tag_tweets(tweet)
+        line[3] = cleaned
+        result.append(line[3][0])
+    return result
+        
+''' This function takes in a file name to write to and an array containing
+    the data to write to the file (for example, an array of tagged tweets)
+    and writes to the file
+'''
+def toFile(fileName, data):
+    result = open(fileName, "w")
+    for element in data:
+        result.write(str(element))
+        result.write('\n')
+    result.close()
+
+
+# Testing ttp parser       
+'''        
 p = ttp.Parser()
 result = p.parse("@Microsoft 2nd computer with same error!!! #Windows10fail Guess we will shelve this until SP1! http://t.co/QCcHlKuy8Q")
 print result.reply
@@ -215,6 +286,33 @@ print result.lists
 #print result.tags
 
 #twokenize.simpleTokenize("@burnettedmond, you now support #IvoWertzel's tweet parser! https://github.com/edburnett/ @butthead"))
-result = CMUTweetTagger.runtagger_parse("@Microsoft 2nd computer with same error!!! #Windows10fail Guess we will shelve this until SP1! http://t.co/QCcHlKuy8Q")
-print result
+'''
+
+''' ************************* Using CMUTweetTagger ***************************
+    The script that we actually ran to generate the tagged tweet files was 
+    CMUTweetTagger.py because this script was not working, but it works now.
+'''
+'''
+print "check is present",CMUTweetTagger.check_script_is_present()
 print CMUTweetTagger.runtagger_parse(['example tweet 1', 'example tweet 2'])
+#print CMUTweetTagger.check_script_is_present(['example tweet 1', 'example tweet 2'])
+#print CMUTweetTagger.runtagger_parse(['example tweet 1', 'example tweet 2'], run_tagger_cmd="java -XX:ParallelGCThreads=2 -Xmx500m -jar ../../ark-tweet-nlp-0.3.2/ark-tweet-nlp-0.3.2.jar")
+
+#readResults = readerAndWriter.readFile("cleaned/PFTweets.txt")
+readResults = np.loadtxt(fname="cleaned/PFTweets.txt", dtype=str, delimiter = '\n')
+
+print "read results\n",readResults
+print "\n DONE READING \n"
+#print "\nclean tweet\n",cleanTweet(tweets)
+#replacedResults = replaceTaggedTweet(readResults)
+taggedTrain = replaceTaggedTweet(readTrain)
+taggedTest = replaceTaggedTweet(readTest)
+print "\n DONE REPLACING \n"
+#print "\nreplace tagged tweet\t",replacedResults
+#np.savetxt("cleaned/testCleaned.txt",replacedResults)
+#toFile("cleaned/PFTweetsCleaned.txt",replacedResults)
+toFile("cleaned/PFTrainTag.txt", taggedTrain)
+toFile("cleaned/PFTestTag.txt", taggedTest)
+print "\n DONE WRITING \n"
+'''      
+        
